c81e2e3afe1750df5d275a523562dff6ce150cd0
AndrewPatrin <115947375+AndrewPatrin@users.noreply.github.com>
2022-11-17 02:43:48 +0400

    Add filter exeption

--- NewsPaper/newsportal/templatetags/custom_filters.py
+++ NewsPaper/newsportal/templatetags/custom_filters.py
@@ -5,10 +5,13 @@ register = template.Library()
 
 @register.filter()
 def censor(text):
-    wordstocensor = ['fool', 'death', 'suffer']
-    text = re.split('(\W+)', text)
-    for i, j in enumerate(text):
-        for k in wordstocensor:
-            if k in j.lower():
-                text[i] = text[i][0] + '*'*(len(text[i]) - 1)
-    return ''.join(text)
+    if isinstance(text, str):
+        wordstocensor = ['fool', 'death', 'suffer']
+        text = re.split('(\W+)', text)
+        for i, j in enumerate(text):
+            for k in wordstocensor:
+                if k in j.lower():
+                    text[i] = text[i][0] + '*' * (len(text[i]) - 1)
+        return ''.join(text)
+    else:
+        raise Exception("This filter is applicable ony to the <class 'str'>")
